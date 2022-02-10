269bbbda5f779a3112f4d198de57b6f39f1f3621
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2022-02-10 19:04:14 +0530

    Add getter for Boolean Bitmap

--- h2/src/main/org/h2/index/IndexHandler.java
+++ h2/src/main/org/h2/index/IndexHandler.java
@@ -19,37 +19,14 @@ import java.util.*;
 
 public class IndexHandler {
     private static String hashBitIndexName  = "hashBitIndex";
-    private static int[] countTempBitmapArray = new int[]{1, 1, 1, 1, 0, 0, 0, 1};
-//    private static int[] countTempBitmapArray = new int[]{1, 1, 1, 1, 0, 0, 0, 1};
     private static int[] outerAndOrTempBitmapArray = new int[]{1, 1, 1, 1, 0, 0, 0, 1};
-    private static String OR = "OR";
-    private static String AND = "AND";
 
-    public static ArrayList<Integer> getCountOperationIndexes(ArrayList<Expression> expressions) {
-        int i = 0;
-        Column column;
-        String columnName;
-        Table table;
-        Index columnIndex;
-        Aggregate aggregateExpression;
-        ArrayList<Integer> countExpressions = new ArrayList<>();
-        for (Expression expression : expressions) {
-            if (expression instanceof Aggregate) {
-                aggregateExpression = (Aggregate) expression;
-                if (aggregateExpression.getAggregateType().toString().equals((AggregateType.COUNT).toString())) {
-                    column = aggregateExpression.getColumnFromAggregateIndex(0);
-                    columnName = column.getName();
-                    table = column.getTable();
-                    columnIndex = table.getIndexForColumn(column, false, false);
-                    //TODO Combine indexes(type). Handle there are many column for index. For now its true
-                    if (true || columnIndex.indexType.equals(hashBitIndexName) && columnIndex.indexColumns.length == 1) {
-                        countExpressions.add(i);
-                    }
-                }
-            }
-            i++;
+    private static ArrayList<Integer> getBitMapIndices(Column column, Table table) {
+        ArrayList<Integer> integerArray = new ArrayList<>(outerAndOrTempBitmapArray.length);
+        for (int j : outerAndOrTempBitmapArray) {
+            integerArray.add(j);
         }
-        return countExpressions;
+        return integerArray;
     }
 
     //TODO: When there is another funcs with count, there can be errors so handle these (Expression == 1) things or Do count separtly and remove that expression
@@ -74,11 +51,7 @@ public class IndexHandler {
                         (true || columnIndex.indexType.equals(hashBitIndexName) &&
                                 columnIndex.indexColumns.length == 1)) {
                     //Get Bitmap Indices
-                    ArrayList<Integer> integerArray = new ArrayList<Integer>(outerAndOrTempBitmapArray.length);
-                    for (int j : outerAndOrTempBitmapArray) {
-                        integerArray.add(j);
-                    }
-                    return integerArray;
+                    return getBitMapIndices(column, table);
                 }
             }
         }
@@ -130,62 +103,6 @@ public class IndexHandler {
             }
         }
         return results;
-    }
-
-    public static HashMap<Integer, ArrayList<Integer>> getValueForCountOperationWithHashBitIndexes(
-            ArrayList<Integer> countIndexes, ArrayList<Expression> expressions) {
-        HashMap<Integer, ArrayList<Integer>> results = new HashMap<>();
-        Column column;
-        String columnName;
-        Table table;
-        Index columnIndex;
-        Aggregate aggregateExpression;
-        ArrayList<Integer> integerArray;
-        for (int index : countIndexes) {
-            aggregateExpression = (Aggregate) (expressions.get(index));
-            column = aggregateExpression.getColumnFromAggregateIndex(0);
-            columnName = column.getName();
-            table = column.getTable();
-            columnIndex = table.getIndexForColumn(column, false, false);
-            TransactionMap<SearchRow, Value> dataMap = columnIndex.getTransactionMap(null);
-//            get the bitmap indices array;
-//            dataMap.get(column);
-            integerArray = new ArrayList<Integer>(countTempBitmapArray.length);
-            for (int i : countTempBitmapArray) {
-                integerArray.add(i);
-            }
-            results.put(index, (integerArray));
-        }
-        return results;
-    }
-
-    //TODO: Check this
-    public static ArrayList<Integer> combineBitmapsForCountOperations(HashMap<Integer, ArrayList<Integer>> bitmaps) {
-        ArrayList<Integer> combinebitmap = null;
-        ArrayList<Integer> tempmap = null;
-        ArrayList<Integer> values;
-
-        Collection<ArrayList<Integer>> mapValues = bitmaps.values();
-        ArrayList<ArrayList<Integer>> entrySet = new ArrayList<>(mapValues);
-
-        for (ArrayList<Integer> array: entrySet) {
-            values = array;
-            if (combinebitmap == null) {
-                combinebitmap = (ArrayList<Integer>) values.clone();
-                tempmap = (ArrayList<Integer>) values.clone();
-            } else {
-                combinebitmap = new ArrayList<>();
-                for (int i = 0; i< values.size(); i++) {
-                    if(values.get(i) == 1 || tempmap.get(i) == 1) {
-                        combinebitmap.add(1);
-                    } else {
-                        combinebitmap.add(0);
-                    }
-                }
-                tempmap = (ArrayList<Integer>) combinebitmap.clone();
-            }
-        }
-        return combinebitmap;
     }
 
     public String getHashBitIndexName() {
